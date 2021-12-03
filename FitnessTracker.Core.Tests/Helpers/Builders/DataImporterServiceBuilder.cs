35465fc18c2c67e8628c5abd720655212d5ea2ae
Ari Roth <ari.roth@gmail.com>
2021-12-03 02:08:22 -0600

    Added DataImporterService unit tests. (#62)

---
+++ FitnessTracker.Core.Tests/Helpers/Builders/DataImporterServiceBuilder.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using FitnessTracker.Core.ImportPreparer.Interfaces;
+using FitnessTracker.Core.Models;
+using FitnessTracker.Core.Services.Implementations;
+using FitnessTracker.Core.Services.Interfaces;
+using Moq;
+
+namespace FitnessTracker.Core.Tests.Helpers.Builders
+{
+	public class DataImporterServiceBuilder : IBuilder<IDataImporterService>
+	{
+		private Mock<IDatabaseService> _databaseServiceMock;
+		private Mock<IImportPreparerFactory> _importPreparerFactoryMock;
+		private Mock<IImportPreparer> _importPreparerMock;
+
+		public static int ImportedRecordsCount => 50;
+
+		public IDatabaseService DatabaseService
+		{
+			get;
+			set;
+		}
+
+		public IImportPreparerFactory ImportPreparerFactory
+		{
+			get;
+			set;
+		}
+
+		public DataImporterServiceBuilder()
+		{
+			SetupMocks(true);
+		}
+
+		public IDataImporterService Build()
+		{
+			return new DataImporterService(DatabaseService, ImportPreparerFactory);
+		}
+
+		public IDataImporterService BuildWithNoImportPreparer()
+		{
+			SetupMocks(false);
+			return new DataImporterService(DatabaseService, ImportPreparerFactory);
+		}
+
+		public void VerifyUpsertIsCalled(Times callTimes)
+		{
+			_databaseServiceMock.Verify(svc => svc.UpsertRecords(It.IsAny<IEnumerable<DailyRecord>>()), callTimes);
+		}
+
+		public void VerifyGetImporterFactoryIsCalled(Times callTimes)
+		{
+			_importPreparerFactoryMock.Verify(f => f.GetImportPreparer(It.IsAny<FileType>()), callTimes);
+		}
+
+		public void VerifyGetRecordsIsCalled(Times callTimes)
+		{
+			_importPreparerMock.Verify(p => p.GetRecords(It.IsAny<string>()), callTimes);
+		}
+
+		private void SetupMocks(bool includePreparer)
+		{
+			_databaseServiceMock = new Mock<IDatabaseService>();
+			_importPreparerFactoryMock = new Mock<IImportPreparerFactory>();
+			_importPreparerMock = new Mock<IImportPreparer>();
+
+			if (includePreparer)
+			{
+				_importPreparerMock.Setup(m => m.GetRecords(It.IsAny<string>()))
+					.Returns(Task.FromResult((IEnumerable<DailyRecord>)TestDataGenerator.GenerateRandomRecords(ImportedRecordsCount)));
+
+				_importPreparerFactoryMock.Setup(m => m.GetImportPreparer(It.IsAny<FileType>()))
+					.Returns(_importPreparerMock.Object);
+			}
+
+			DatabaseService = _databaseServiceMock.Object;
+			ImportPreparerFactory = _importPreparerFactoryMock.Object;
+		}
+	}
+}
---
+++ FitnessTracker.Core.Tests/Services/DataImporterServiceTests.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FitnessTracker.Core.Services.Interfaces;
+using FitnessTracker.Core.Tests.Helpers.Builders;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace FitnessTracker.Core.Tests.Services
+{
+	// This class doesn't need to test as much as others, because it basically exists to call
+	// the combination of ImportPreparerFactory, ImportPreparer, and DatabaseService.  All three
+	// of those are more comprehensively tested elsewhere.
+	public class DataImporterServiceTests : TestBase<IDataImporterService, DataImporterServiceBuilder>
+	{
+		[TestInitialize]
+		public virtual void InitializeTest()
+		{
+			Builder = new DataImporterServiceBuilder();
+			Target = Builder.Build();
+		}
+
+		[TestClass]
+		public class ConstructorTests : DataImporterServiceTests
+		{
+			[TestInitialize]
+			public override void InitializeTest()
+			{
+				// Only need a builder here; these tests don't need a database
+				Builder = new DataImporterServiceBuilder();
+			}
+
+			[TestMethod]
+			public void Should_Initialize_Correctly()
+			{
+				_ = Builder.Build();
+			}
+
+			[TestMethod]
+			[ExpectedException(typeof(ArgumentNullException))]
+			public void Should_Fail_With_Null_Database_Service()
+			{
+				Builder.DatabaseService = null;
+				_ = Builder.Build();
+			}
+
+			[TestMethod]
+			[ExpectedException(typeof(ArgumentNullException))]
+			public void Should_Fail_With_Null_Import_Preparer_Factory()
+			{
+				Builder.ImportPreparerFactory = null;
+				_ = Builder.Build();
+			}
+		}
+
+		[TestClass]
+		public class ImportDataTests : DataImporterServiceTests
+		{
+			private const string FILE_NAME = "some-file.txt";
+
+			[TestMethod]
+			public async Task Should_Import_Successfully()
+			{
+				// Make sure we pass the File.Exists check.  The actual file doesn't matter since
+				// it's mocked.
+				File.WriteAllText(FILE_NAME, "Lorem ipsum dolor sit amet.");
+				var result = await Target.ImportData(FILE_NAME);
+				File.Delete(FILE_NAME);
+
+				Assert.AreEqual(DataImporterServiceBuilder.ImportedRecordsCount, result, "Returned the wrong count of records.");
+				Builder.VerifyGetImporterFactoryIsCalled(Times.Once());
+				Builder.VerifyGetRecordsIsCalled(Times.Once());
+				Builder.VerifyUpsertIsCalled(Times.Once());
+			}
+
+			[TestMethod]
+			[ExpectedException(typeof(InvalidOperationException))]
+			public async Task Should_Fail_On_Empty_FileName()
+			{
+				_ = await Target.ImportData(string.Empty);
+			}
+
+			[TestMethod]
+			[ExpectedException(typeof(ArgumentNullException))]
+			public async Task Should_Fail_On_Null_FileName()
+			{
+				_ = await Target.ImportData(null);
+			}
+
+			[TestMethod]
+			[ExpectedException(typeof(FileNotFoundException))]
+			public async Task Should_Fail_On_Non_Existent_File()
+			{
+				_ = await Target.ImportData(FILE_NAME);
+			}
+
+			[TestMethod]
+			[ExpectedException(typeof(InvalidOperationException))]
+			public async Task Should_Fail_On_No_Preparer_Found()
+			{
+				Target = Builder.BuildWithNoImportPreparer();
+
+				try
+				{
+					File.WriteAllText(FILE_NAME, "Lorem ipsum dolor sit amet.");
+					_ = await Target.ImportData(FILE_NAME);
+				}
+				finally
+				{
+					File.Delete(FILE_NAME);
+				}
+			}
+		}
+	}
+}
